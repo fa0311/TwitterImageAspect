506b6c2e545044f28d562c361b4d0e20226d3a0e
fa0311 <yukinashi2742756@yahoo.co.jp>
2021-05-06 19:38:38 +0900

    fix #1

--- js/main.js
+++ js/main.js
@@ -3,17 +3,28 @@ setInterval(function() {
     images.each(function(key, value) {
         let image = images.eq(key);
         let parent = image.parent();
-        if (parent.find(".css-1dbjc4n.r-18u37iz.r-1pi2tsx.r-13qz1uu").length == 0 && parent.find("img").height() / 9 > parent.find("img").width() / 16) {
+        if (parent.find("img").attr("loaded") === "true") {
+            return true;
+        }
+        if (parent.find(".css-1dbjc4n.r-18u37iz.r-1pi2tsx.r-13qz1uu").length !== 0) {
+            parent.find("img").attr("loaded", "true");
+            return true;
+        }
+        image.css("padding-bottom", "100%");
+        if (parent.find("img").height() === undefined || parent.find("img").width() === undefined || parent.find("img").height() === 0 || parent.find("img").width() === 0) {
+            return true;
+        }
+        parent.find("img").attr("loaded", "true");
+        if (parent.find("img").height() / 9 > parent.find("img").width() / 16) {
+            image.css("padding-bottom", "100%");
             if (parent.find("img").height() / 4 > parent.find("img").width() / 3) {
                 image.css("padding-bottom", "132%");
             } else {
                 image.css("padding-bottom", parent.find("img").height().toString() + "px");
             }
+        } else {
+            image.css("padding-bottom", "56.25%");
         }
-        let box = parent.find(".css-1dbjc4n.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-61z16t.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010");
-        box.css("margin-bottom", "0px");
-        box.css("margin-left", "0px");
-        box.css("margin-top", "0px")
-        box.css("margin-right", "0px");
+        parent.find(".css-1dbjc4n.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010").attr("style", "");
     });
 }, 500);
